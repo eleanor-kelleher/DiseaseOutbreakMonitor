04619046b257e6a0faae7af4cc542342586319f1
seanBr97 <35768304+seanBr97@users.noreply.github.com>
2021-03-09 00:44:22 +0000

    Backend of Symptoms Activity --- not fully working

--- app/src/main/java/GroupProject/Team8/DiseaseOutbreakMonitor/SymptomsActivity.kt
+++ app/src/main/java/GroupProject/Team8/DiseaseOutbreakMonitor/SymptomsActivity.kt
@@ -2,9 +2,11 @@ package GroupProject.Team8.DiseaseOutbreakMonitor
 
 
 import GroupProject.Team8.DiseaseOutbreakMonitor.adapter.ItemAdapter
+import GroupProject.Team8.DiseaseOutbreakMonitor.adapter.checkBoxStateArray
 import GroupProject.Team8.DiseaseOutbreakMonitor.data.Datasource
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -16,7 +18,6 @@ class SymptomsActivity : AppCompatActivity() {
     var patientSex = ""
     var disease = ""
     var comment = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTitle(R.string.symptom_activity_title)
@@ -33,14 +34,43 @@ class SymptomsActivity : AppCompatActivity() {
 
         // Initialise data
         val myDataset = Datasource().loadSymptoms()
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        var recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
         // improves performance if #items in list does not change during execution
         //recyclerView.setHasFixedSize(true)
     }
 
+    /*
+        Call this at the end of the activity
+     */
+    fun generateSymptomsString (): String {
+
+        val symptoms = Datasource().loadSymptoms()
+        var checkedSymptoms = checkBoxStateArray    // from ItemAdapter
+        val sb = StringBuilder()
+        val size = checkedSymptoms.size()
+        Log.i("size", size.toString())
+        var symptomName = ""
+        var i = 0
+        while (i < size)
+        {
+            var isChecked = checkedSymptoms.get(i, false)
+            if (isChecked)
+            {
+                val symptom = symptoms.get(i)
+                symptomName = getString(symptom.stringResourceId)
+            }
+            sb.append(symptomName)
+            sb.append(" ")
+            i++
+        }
+        return sb.toString()
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        Log.i("printing", generateSymptomsString()) // print symptoms string to logcat console when back button is pressed
         val intent = Intent(applicationContext, DiagnosisActivity::class.java)
         intent.putExtra("DATE", date)
         intent.putExtra("PATIENT_AGE", patientAge)
