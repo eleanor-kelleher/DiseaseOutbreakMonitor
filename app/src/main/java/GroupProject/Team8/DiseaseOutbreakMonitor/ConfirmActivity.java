1ac28b2bdc403df9aba2ecdf133f4ea1e73e16df
James Kelleher <kellehj1@tcd.ie>
2021-03-24 13:01:37 +0000

    TableLayout summary of data entered (GPS still not working)

--- app/src/main/java/GroupProject/Team8/DiseaseOutbreakMonitor/ConfirmActivity.java
+++ app/src/main/java/GroupProject/Team8/DiseaseOutbreakMonitor/ConfirmActivity.java
@@ -10,14 +10,15 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -26,10 +27,13 @@ public class ConfirmActivity extends AppCompatActivity {
     private static final int PERMISSIONS_COARSE_LOCATION = 99;
     long date;
     PatientModel patient = new PatientModel();
+    TextView textViewAge, textViewSex, textViewTemperature, textViewBloodPressure,
+            textViewSymptoms, textViewComments, textViewDiagnosis;
 
     // Google's API for location services
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
+    LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +53,20 @@ public class ConfirmActivity extends AppCompatActivity {
         patient.setBloodPressureDiastolic(intent.getIntExtra(Constants.BP_DIASTOLIC, -1));
         patient.setDisease(intent.getStringExtra(Constants.HW_DIAGNOSIS));
         patient.setSymptoms(intent.getStringExtra(Constants.SYMPTOMS));
-        if(intent.getStringExtra(Constants.COMMENT) == null) {
+        if (intent.getStringExtra(Constants.COMMENT) == null) {
             patient.setComment("");
-        }
-        else {
+        } else {
             patient.setComment(intent.getStringExtra(Constants.COMMENT));
         }
+
+        textViewAge = findViewById(R.id.textAgeFillable);
+        textViewSex = findViewById(R.id.textSexFillable);
+        textViewTemperature = findViewById(R.id.textTempFillable);
+        textViewBloodPressure = findViewById(R.id.textBPFillable);
+        textViewSymptoms = findViewById(R.id.textSymptomsFillable);
+        textViewComments = findViewById(R.id.textCommentFillable);
+        textViewDiagnosis = findViewById(R.id.textDiagnosisFillable);
+        fillSummaryTable();
 
         patient.setDate(date);
 
@@ -62,11 +74,18 @@ public class ConfirmActivity extends AppCompatActivity {
         locationRequest.setInterval(30000);
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                saveLatAndLong(locationResult.getLastLocation());
+            }
+        };
 
-        updateGPS();
+        // updateGPS();
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = new Intent(getApplicationContext(), SymptomsActivity.class);
         intent.putExtra(Constants.AGE, patient.getAge());
         intent.putExtra(Constants.SEX, patient.getSex());
@@ -84,10 +103,9 @@ public class ConfirmActivity extends AppCompatActivity {
 
         switch (requestCode) {
             case PERMISSIONS_COARSE_LOCATION:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     updateGPS();
-                }
-                else {
+                } else {
                     Toast.makeText(this, "Please enable GPS so this patient data can be stored", Toast.LENGTH_SHORT).show();
                 }
         }
@@ -95,12 +113,12 @@ public class ConfirmActivity extends AppCompatActivity {
 
     private void updateGPS() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(ConfirmActivity.this);
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
-                    patient.setLatitude(location.getLatitude());
-                    patient.setLongitude(location.getLongitude());
+                    saveLatAndLong(location);
+
                 }
             });
         }
@@ -111,10 +129,28 @@ public class ConfirmActivity extends AppCompatActivity {
         }
     }
 
-    public void addToDatabase(View view) {
+    private void saveLatAndLong(Location location) {
+        patient.setLatitude(location.getLatitude());
+        patient.setLongitude(location.getLongitude());
+    }
+
+    public void fillSummaryTable() {
+        textViewAge.setText(String.valueOf(patient.getAge()));
+        textViewSex.setText(patient.getSex());
+        textViewTemperature.setText(String.valueOf(patient.getTemperatureCelsius()));
+        int bpSystolic = patient.getBloodPressureSystolic();
+        int bpDiastolic = patient.getBloodPressureDiastolic();
+        String bloodPressure = bpSystolic + "/" + bpDiastolic;
+        textViewBloodPressure.setText(bloodPressure);
+        textViewSymptoms.setText(patient.getSymptoms().replace(",", ", "));
+        textViewComments.setText(patient.getComment());
+        textViewDiagnosis.setText(patient.getDisease());
+    }
+
+    private void addToDatabase(View view) {
         DatabaseHelper dbHelper = new DatabaseHelper(ConfirmActivity.this);
         if(dbHelper.addOne(patient)) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
         else {
